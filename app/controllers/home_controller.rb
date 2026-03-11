b6e1be1a5207c8cfb3139087e2afc4adb77d880d
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-12 00:01:47 +0100

    start working on page design!!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if session[:user_id] == nil 
-      render "guest"
-    else
-      @hackvertisements = Hackvertisement.where("user_id":session[:user_id]["uid"])
-      render "index"
-    end
+    @logged_in = session[:user_id] != nil 
+    render "index"
   end
 end
