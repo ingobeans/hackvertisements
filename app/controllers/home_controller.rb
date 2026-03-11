238bb9ffdda24021d6f35c0f27bb492fd5b01520
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-11 20:35:48 +0100

    show user's uploaded hackvertisements!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     if session[:user_id] == nil 
       render "guest"
     else
+      @hackvertisements = Hackvertisement.where("user_id":session[:user_id]["uid"])
       render "index"
     end
   end
