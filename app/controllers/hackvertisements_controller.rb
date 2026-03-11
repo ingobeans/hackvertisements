879b49e9616415a2164f16f668b8e4c17b3fc45c
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-11 20:37:07 +0100

    make hackvertisements deletion redirect to root_path

--- app/controllers/hackvertisements_controller.rb
+++ app/controllers/hackvertisements_controller.rb
@@ -89,7 +89,7 @@ class HackvertisementsController < ApplicationController
     @hackvertisement.destroy!
 
     respond_to do |format|
-      format.html { redirect_to hackvertisements_path, notice: "Hackvertisement was successfully destroyed.", status: :see_other }
+      format.html { redirect_to root_path, notice: "Hackvertisement was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
