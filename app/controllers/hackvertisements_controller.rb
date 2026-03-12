e3e8dba4ad7ddd330801ac2803fe740d10e7009d
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-12 21:37:22 +0100

    make destroying hackvertisement redirect to dashboard

--- app/controllers/hackvertisements_controller.rb
+++ app/controllers/hackvertisements_controller.rb
@@ -90,7 +90,7 @@ class HackvertisementsController < ApplicationController
     @hackvertisement.destroy!
 
     respond_to do |format|
-      format.html { redirect_to root_path, notice: "Hackvertisement was successfully destroyed!", status: :see_other }
+      format.html { redirect_to dashboard_path, notice: "Hackvertisement was successfully destroyed!", status: :see_other }
       format.json { head :no_content }
     end
   end
